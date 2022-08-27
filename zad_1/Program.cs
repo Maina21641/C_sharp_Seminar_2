d462cb01ae930d8057f8a89112c37c677cc53c33
AlexNurzhanov <turk.alek@yandex.ru>
2022-08-27 23:08:12 +0400

    zad_1 -сделал алгоритм убирающий второе знак в трехначном числе, создал папку zad_2 с алгоритмом проверяющем кратность чисел

--- zad_1/Program.cs
+++ zad_1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int randomNumber = new Random().Next(10,1000);
+
+int result = (randomNumber / 100) * 10 + randomNumber % 10;
+
+System.Console.WriteLine($"число до преобразований - {randomNumber}");
+System.Console.WriteLine($"число до преобразований - {result}");
---
+++ zad_2/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = 12;
+int b = 4;
+
+if (a % b == 0){
+    System.Console.WriteLine("Кратно");
+}
+else{
+    System.Console.WriteLine($"Не кратно, остаток - {a % b}");
+}
