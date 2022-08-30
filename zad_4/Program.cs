92c41380677d11c5b4457a953d9788d0c72274fa
Alex <Yulien.ru@Yandex.ru>
2022-08-30 13:32:16 +0400

    добавлено решение проверки, является ли одно число квадратом другого

---
+++ zad_4/Program.cs
@@ -0,0 +1,13 @@
+﻿bool check (int a, int b)
+{
+     if (a * a == b){
+        return true;
+    } 
+    else if (b * b == a){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+System.Console.WriteLine(check (7, 14));
